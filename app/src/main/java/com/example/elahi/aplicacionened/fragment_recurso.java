9b70d852d59c223e59018d3fefbdebc151a82c6f
elahito <elahigironcueto@gmail.com>
2019-01-27 14:53:18 -0600

    los recursos y noticias ahora se obtienen desde la bd

--- app/src/main/java/com/example/elahi/aplicacionened/fragment_recurso.java
+++ app/src/main/java/com/example/elahi/aplicacionened/fragment_recurso.java
@@ -1,10 +1,15 @@
 package com.example.elahi.aplicacionened;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +20,12 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,21 +34,132 @@ import java.util.List;
  * A simple {@link Fragment} subclass.
  */
 public class fragment_recurso extends Fragment {
-    private List<clase_recurso> Recurso = new ArrayList<clase_recurso>();
+    private static List<clase_recurso> Recurso = new ArrayList<clase_recurso>();
+    private static List<String> urls=new ArrayList<>();
 
     View view;
+    public static String texto1;
+    public static String sup1;
+    public static String sup2;
+    private static String DEBUG_TAG;
+
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        buscar();
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.fragment_recurso, container, false);
-        Recurso();
+
         RecursoView();
         return view;
     }
 
-    private void Recurso() {
+
+    public static void imprime(String aux) {
+        String[] parts = aux.split(";");
+        Recurso.clear();
+        urls.clear();
+        for(int i=0;i<parts.length-1;i++){
+            if (!parts[i+1].equals("")) {
+                String[] parts2=parts[i].split("&");
+                sup1=parts2[0];
+                sup2=parts2[1];
+                Recurso(sup1);
+                Url(sup2);
+            }
+        }
+    }
+
+    public  boolean isConected(Context context) {
+
+        boolean connected = false;
+
+        ConnectivityManager connec = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Recupera todas las redes (tanto móviles como wifi)
+        NetworkInfo[] redes = connec.getAllNetworkInfo();
+
+        for (int i = 0; i < redes.length; i++) {
+            // Si alguna red tiene conexión, se devuelve true
+            if (redes[i].getState() == NetworkInfo.State.CONNECTED) {
+                connected = true;
+            }
+        }
+        return connected;
+    }
+
+    public void buscar(){
+        //DEBUG_TAG= "https://elahito.000webhostapp.com/php/conection.php";
+        DEBUG_TAG= "http://192.168.1.66:8888/conection_rec.php/";
+        new fragment_recurso.DownloadWebpageTask().execute(DEBUG_TAG);
+
+    }
+
+    private static class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "No se puede recuperar la página web. URL puede no ser válida.";
+            }
+        }
+
+        // onPostExecute muestra el resultado de AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            imprime(result);
+        }
+    }
+
+    private static String downloadUrl(String myurl) throws IOException {
+        InputStream is = null;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milisegundos */);
+            conn.setConnectTimeout(15000 /* milisegundos */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Inicia la consulta
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d( DEBUG_TAG,"La respuesta es: " + response);
+            is = conn.getInputStream();
+            //Para descargar la página web completa
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            String webPage = "";
+            String data = "";
+            while ((data = reader.readLine()) != null) {
+                webPage += data + "\n";
+            }
+            return webPage;
+            // Se asegura de que el InputStream se cierra después de la aplicación deja de usarla.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    private static void Recurso(String encab){
+        Recurso.add(new clase_recurso (R.drawable.folder,encab));
+    }
+
+    private static void Url(String url){
+        urls.add(url);
+    }
+
+
+    /*private void Recurso() {
         //JORNADA1
         Recurso.add(new clase_recurso(R.drawable.folder,"DISTRIBUCION DE AULAS PARA ENTREGA DE GAFETES"));
         Recurso.add(new clase_recurso(R.drawable.folder,"REGLAMENTO DEL COMEDOR"));
@@ -50,7 +172,7 @@ public class fragment_recurso extends Fragment {
         Recurso.add(new clase_recurso(R.drawable.folder,"PROGRAMACIÓN SÓFTBOL"));
         Recurso.add(new clase_recurso(R.drawable.folder,"PROGRAMACIÓN AJEDREZ"));
         Recurso.add(new clase_recurso(R.drawable.folder,"PROGRAMACIÓN NATACIÓN"));
-    }
+    }*/
 
     private void RecursoView(){
         ArrayAdapter<clase_recurso> adapter=new MyListAdapter();
@@ -62,83 +184,13 @@ public class fragment_recurso extends Fragment {
 
 
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (position==0){
-                    //distribucion
-                    Uri uri = Uri.parse("https://drive.google.com/open?id=1qHBU1j0Osb88AKmBxpqbX2UxJHTkdam6");
+                for(int i=0; i<=position; i++){
+                    if (position==i){
+                    String cadena = urls.get(i).toString();
+                    Uri uri = Uri.parse(cadena);
                     Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                     startActivity(intent);
-                }
-
-                if (position==1){
-                    //reglamento
-                    Uri uri = Uri.parse("https://drive.google.com/open?id=1NVZdZ8pcIKNxv2teBrzuXFRo966bBfHn");
-                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                    startActivity(intent);
-                }
-
-                if (position==2){
-                    //futbol
-                    Uri uri = Uri.parse("https://drive.google.com/file/d/1wnIe3q2jWN-ttV9NM-DT_usSlIQ8Fd2L/view?usp=sharing");
-                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                    startActivity(intent);
-                }
-
-                if (position==3){
-                    //basquetbol
-                    Uri uri = Uri.parse("https://drive.google.com/open?id=1QWbiSSiAA6vkMH1rvOOuS52VRnn83HoL");
-                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                    startActivity(intent);
-                }
-
-                if(position==4){
-                    //beisbol
-                    Uri uri = Uri.parse("https://drive.google.com/open?id=17auiRjPsaxpdumMMhDItqWboCYhkXzqU");
-                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                    startActivity(intent);
-                }
-
-                if (position==5){
-                    //voleibol sala
-                    Uri uri = Uri.parse("https://drive.google.com/open?id=1L1dJ3dD4KM5K3LIYRQKn38hUvI7DaNEK");
-                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                    startActivity(intent);
-                }
-
-                if(position==6){
-                    //voleibol playa
-                    Uri uri = Uri.parse("https://drive.google.com/open?id=1yiOgdUYlFA96RuKAy18BaHfYHWeUyND3");
-                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                    startActivity(intent);
-                }
-
-                if(position==7){
-                    //atletismo
-                    Uri uri = Uri.parse("https://drive.google.com/open?id=1-JyrrPlfePV40y9yAwYEmqPz6KCPHGg8");
-                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                    startActivity(intent);
-                }
-
-                if(position==8){
-                    //softbol
-                    Uri uri = Uri.parse("https://drive.google.com/open?id=1awBybuUrgfqiYTnRsxf-WLUJte32hH9D");
-                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                    startActivity(intent);
-                }
-
-                if(position==9){
-                    //ajedrez
-                    Uri uri = Uri.parse("https://drive.google.com/open?id=1uPbJuNLhNUE08XCuXJIpv5rP9sBUbTQ6");
-                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                    startActivity(intent);
-                }
-
-                if(position==10){
-                    //NATACIÓN
-                    Uri uri = Uri.parse("https://drive.google.com/open?id=1QQzYtf1hTAN7PrwrvbpjwAqkCl-0JpFg");
-                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                    startActivity(intent);
-
-                }
+                }}
             }
         });
     }
